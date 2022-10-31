449dd77a2c65713cade1a20a6b740f38c7b58b6a
Robert Davis <spahatticat246@gmail.com>
2022-10-31 16:51:33 -0400

    redo vscode config and first build

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// #include <ifstream>
+#include <curl/curl.h>
+
+using namespace std;
+
+int main(int argc, char *argv[] ) {
+    if(argc == 1) {
+        cout << "usage: " << argv[0] << " [ARGUMENT]" << endl;
+        return 0;
+    }
+}
