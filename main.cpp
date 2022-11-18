33e47af9e2a9863f7694e47d0d23362642df6f50
Robert Davis <spahatticat246@gmail.com>
2022-11-18 16:24:33 -0500

    add help message and packer shell

--- main.cpp
+++ main.cpp
@@ -10,13 +10,15 @@ int main(int argc, char *argv[] ) {
     string input;
     cout << "Welcome to packer the GNU/Linux Package manager\ntype help for help\n\n" << endl;
     while(true) {
-        cout << "[packer] >>> ";
-        cin >> input;
-        if(input == "exit") {
-            cout << "Bye!" << endl;
-            system("sleep 1");
-            break;
-        }
+		cout << "[packer] >>> ";
+		cin >> input;
+		if(input == "exit") {
+			cout << "Bye!" << endl;
+			break;
+        	}
+		if(input == "help") {
+			cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nexit: exit the program" << endl;
+		}
     }
     return 0;
 }
