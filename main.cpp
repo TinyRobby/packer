99e74f1316d58e658fac1c08f64ba8d890d783b4
Robert Davis <spahatticat246@gmail.com>
2022-12-07 06:12:52 -0500

    gfuyfu

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-// #include <curl/curl.h>
+#include <curl/curl.h>
 #include "json.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[] ) {
+	curl_global_init(1);
+	CURL *easy_handle = curl_easy_init();
+	if(!easy_handle) {
+		cerr << "Error: cURL didn't load" << endl;
+		return 1;
+	}
     string input;
     cout << "Welcome to packer the GNU/Linux Package manager\ntype help for help\n\n" << endl;
     while(true) {
 		cout << "[packer] >>> ";
 		cin >> input;
 		if(input == "exit") {
-			cout << "Bye!" << endl;
-			break;
+				cout << "Bye!" << endl;
+				break;
         	}
 		if(input == "help") {
 			cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit: exit the program" << endl;
