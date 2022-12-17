e08aab7ce498e5b820b43f66e3eeb06fadfa7d09
Robert Davis <spahatticat246@gmail.com>
2022-12-17 18:45:58 -0500

    bcvcfghhfgh

--- Makefile
+++ Makefile
@@ -1,14 +1,4 @@
-packer: main.o
-	g++ -Wall  obj/main.o -lcurl -o bin/packer
-
-replit-packer: main.o
-	g++ -Wall obj/main.o -o bin/replit/packer
-
-main.o: main.cpp
-	g++ -c main.cpp -lcurl -o obj/main.o
-
-run: 
-	bin/packer
+# Don't use this! It's an old Makefile
 packer: main.o
 	g++ -Wall  obj/main.o -lcurl -o bin/packer
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <stdlib.h>
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <curl/curl.h>
-#include "json.hpp"
+#include "Headers/json.hpp"
 
 using namespace std;
 
@@ -17,13 +18,17 @@ int main(int argc, char *argv[] ) {
     cout << "Welcome to packer the GNU/Linux Package manager\ntype help for help\n\n" << endl;
     while(true) {
 		cout << "[packer] >>> ";
-		cin >> input;
-		if(input == "exit") {
-				cout << "Bye!" << endl;
-				break;
-        	}
-		if(input == "help") {
-			cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit: exit the program" << endl;
+		getline(cin, input);
+		if(input.empty() == false) {
+			if(input == "exit") {
+					cout << "Bye!" << endl;
+					break;
+				}
+			if(input == "help") {
+				cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit: exit the program" << endl;
+			}
+		} else {
+			cout << "No command specified" << endl;
 		}
     }
     return 0;
