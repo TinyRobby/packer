9371a6463089ad8fe3a261870c0b380329abad1e
Robert Davis <spahatticat246@gmail.com>
2022-12-27 11:13:31 -0500

    easy read code now

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@
 using namespace std;
 
 int main(int argc, char *argv[] ) {
+	// cURL setup
 	curl_global_init(1);
 	CURL *easy_handle = curl_easy_init();
 	if(!easy_handle) {
 		cerr << "Error: cURL didn't load" << endl;
 		return 1;
 	}
+	// Get commands
     string input;
     cout << "Welcome to packer the GNU/Linux Package manager\ntype help for help\n\n" << endl;
     while(true) {
@@ -25,8 +27,9 @@ int main(int argc, char *argv[] ) {
 					break;
 				}
 			if(input == "help") {
-				cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit: exit the program" << endl;
+				cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit/quit: exit the program\ngen-config: generate a config file if none is present\ndevmode: engage devmode (single session only)" << endl;
 			}
+			if(input == "devmode")
 		} else {
 			cout << "No command specified" << endl;
 		}
