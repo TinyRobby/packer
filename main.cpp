9238800c2a788d581671b54e3fa9e3efc90d0307
Robert Davis <spahatticat246@gmail.com>
2023-01-16 11:33:47 -0500

    Debug

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 #include <fstream>
 #include <curl/curl.h>
 #include "Headers/json.hpp"
+#include <stumpless.h>
 
 using namespace std;
 
 int main(int argc, char *argv[] ) {
+	stumpless_target* file_target = stumpless_open_file_target("packer.log");
+	stump("Packer started"); 
 	// cURL setup
 	curl_global_init(1);
 	CURL *easy_handle = curl_easy_init();
@@ -26,10 +29,24 @@ int main(int argc, char *argv[] ) {
 					cout << "Bye!" << endl;
 					break;
 				}
-			if(input == "help") {
-				cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit/quit: exit the program\ngen-config: generate a config file if none is present\ndevmode: engage devmode (single session only)" << endl;
+			if(input == "install") {
+				cout << "install from machine or internet?" << endl;
+				
 			}
-			if(input == "devmode")
+			if(input == "help") {
+				cout << "Format:\n [Command]: [Short Desc]\nhelp: this help message\ninstall: install a program\nremove: remove a program\nexit/quit: exit the program\ngen-config: generate a config file if none is present\n" << endl;
+			}
+			if(input == "devmode") {
+				char choose;
+				cout << "Enable devmode? (y,n)";
+				cin >> choose;
+				if(choose == 'y') {
+					cout << "devmode enabled\n";
+				}
+				else {
+					cout << "devmode disabled\n";
+				}
+			}
 		} else {
 			cout << "No command specified" << endl;
 		}
